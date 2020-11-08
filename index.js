d8bce7506bc90b07c86f4be91a83bf2d170bf642
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-11-08 10:25:30 +0100

    Auto Commit

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ module.exports = function(config) {
       });
       worker.on('error', function(e) {
         console.log('Error in Worker',e);
+        worker2 = false;
         _spawnCleanerWorker();
       });
       worker.on('exit', (code) => {
         console.log('Cleaner finished with Code',code);
+        worker2 = false;
         _spawnCleanerWorker();
       });
     } else return;
@@ -55,47 +57,51 @@ module.exports = function(config) {
         }
         resolve();
       } else {
-      db.serialize(function() {
-          db.run("CREATE TABLE IF NOT EXISTS 'archive_"+msg.uuid+"' (time INTEGER PRIMARY KEY,last24h_price REAL,last7d_price REAL,last30d_price REAL,last90d_price REAL,last180d_price REAL,last365d_price REAL)");
-          let cols = [];
-          let values = [];
-          cols.push("time");
-          values.push(msg.time);
+        if(!worker2) {
+        db.serialize(function() {
+            db.run("CREATE TABLE IF NOT EXISTS 'archive_"+msg.uuid+"' (time INTEGER PRIMARY KEY,last24h_price REAL,last7d_price REAL,last30d_price REAL,last90d_price REAL,last180d_price REAL,last365d_price REAL)");
+            let cols = [];
+            let values = [];
+            cols.push("time");
+            values.push(msg.time);
 
-          if(typeof msg.stats.last24h !== 'undefined') {
-              cols.push('last24h_price');
-              values.push(msg.stats.last24h.energyPrice_kwh);
-          }
+            if(typeof msg.stats.last24h !== 'undefined') {
+                cols.push('last24h_price');
+                values.push(msg.stats.last24h.energyPrice_kwh);
+            }
 
-          if(typeof msg.stats.last7d !== 'undefined') {
-              cols.push('last7d_price');
-              values.push(msg.stats.last7d.energyPrice_kwh);
-          }
+            if(typeof msg.stats.last7d !== 'undefined') {
+                cols.push('last7d_price');
+                values.push(msg.stats.last7d.energyPrice_kwh);
+            }
 
-          if(typeof msg.stats.last30d !== 'undefined') {
-              cols.push('last30d_price');
-              values.push(msg.stats.last30d.energyPrice_kwh);
-          }
+            if(typeof msg.stats.last30d !== 'undefined') {
+                cols.push('last30d_price');
+                values.push(msg.stats.last30d.energyPrice_kwh);
+            }
 
-          if(typeof msg.stats.last90d !== 'undefined') {
-              cols.push('last90d_price');
-              values.push(msg.stats.last90d.energyPrice_kwh);
-          }
+            if(typeof msg.stats.last90d !== 'undefined') {
+                cols.push('last90d_price');
+                values.push(msg.stats.last90d.energyPrice_kwh);
+            }
 
-          if(typeof msg.stats.last180d !== 'undefined') {
-              cols.push('last180d_price');
-              values.push(msg.stats.last180d.energyPrice_kwh);
-          }
+            if(typeof msg.stats.last180d !== 'undefined') {
+                cols.push('last180d_price');
+                values.push(msg.stats.last180d.energyPrice_kwh);
+            }
 
-          if(typeof msg.stats.last365d !== 'undefined') {
-              cols.push('last365d_price');
-              values.push(msg.stats.last365d.energyPrice_kwh);
+            if(typeof msg.stats.last365d !== 'undefined') {
+                cols.push('last365d_price');
+                values.push(msg.stats.last365d.energyPrice_kwh);
+            }
+            try {
+              db.run("INSERT into 'archive_"+msg.uuid+"' ("+cols.concat()+")  VALUES ("+values.concat()+")");
+            } catch(e) {}
+            resolve();
+            });
+          } else {
+            resolve();
           }
-          try {
-            db.run("INSERT into 'archive_"+msg.uuid+"' ("+cols.concat()+")  VALUES ("+values.concat()+")");
-          } catch(e) {}
-          resolve();
-          });
         }
 
     });
